89b72d46994e4317bb73ae7c4120eccc3e139a05
hasnatulhaq <hasnatulhaq@outlook.com>
2022-07-19 11:30:26 +0500

    adding maps

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,11 +1,21 @@
 import './Map.css'
+import ReactMapGl from "react-map-gl";
 
-
+const MAPBOX_TOKEN = "pk.eyJ1IjoiaGFzbmF0dWxoYXEiLCJhIjoiY2wwdzBjb3JrMTc3ajNkbjUyaDljbG8zcyJ9.zR9o-L0WGPt1JKTHd0oUFg";
 
 function Map(){
     return(
         <>
-           <h1>map here</h1>
+        <ReactMapGl
+        width="100vw"
+        height="100vh"
+        style={{ borderTop: "5px solid #245c7c" }}
+        mapStyle={"mapbox://styles/hasnatulhaq/cl1kc4e5o00my14o3kuifx4vp"}
+        mapboxAccessToken={MAPBOX_TOKEN}
+        // {...viewport}
+        // onMove={(evt) => setviewport(evt.viewport)}
+        // onClick={displaydata}
+      ></ReactMapGl>
         </>
     )
 }
