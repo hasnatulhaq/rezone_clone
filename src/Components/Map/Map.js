90617f25385bded3a5f7f7b77903b206e6c57c52
hasnatulhaq <hasnatulhaq@outlook.com>
2022-07-20 14:25:27 +0500

    setting map design

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -39,7 +39,7 @@ function Map(){
           width="100%"
           height="100%"
         />
-         <NavigationControl />
+         <NavigationControl position='top-left' />
       </ReactMapGl>
         </>
     )
