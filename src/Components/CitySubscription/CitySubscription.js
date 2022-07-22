3818edbeeeae8b5adfd7a936c488e4c14d4d77f1
hasnatulhaq <hasnatulhaq@outlook.com>
2022-07-22 12:23:54 +0500

    city subscription dropdown

--- src/Components/CitySubscription/CitySubscription.js
+++ src/Components/CitySubscription/CitySubscription.js
@@ -5,9 +5,16 @@ import './CitySubscription.css'
 function CitySubscription(){
     return(
            <div className='layouut-d'>
+            <div className='citysub'>
+            <p>City Subscription</p>
+            <div className='citysub-item'>
+            <input type="text"></input>
+            <input type="text"></input>
+            <button className='citysub-btn'>Submit</button>
+            </div>
+            </div>
            <Table/>
            </div>
-            
     )
 }
 
