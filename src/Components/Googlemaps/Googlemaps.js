c8ec74b9313085e27714ae52e0c41188c6959c39
hasnatulhaq <hasnatulhaq@outlook.com>
2022-08-15 17:13:14 +0500

    changing colors of zone code

--- src/Components/Googlemaps/Googlemaps.js
+++ src/Components/Googlemaps/Googlemaps.js
@@ -1,10 +1,11 @@
-import React, {useState } from 'react'
+import React, {useEffect, useState } from 'react'
 import './Googlemaps.css'
 import { GoogleMap, useJsApiLoader , Autocomplete, Marker} from '@react-google-maps/api';
-// import {GoogleMapsOverlay as DeckOverlay} from '@deck.gl/google-maps';
-// import axios from 'axios'
+//import {GoogleMapsOverlay as DeckOverlay} from '@deck.gl/google-maps';
+import axios from 'axios'
 import { GoogleMapsOverlay } from "@deck.gl/google-maps";
 import { MVTLayer } from "deck.gl";
+import { staticColor } from "../../Color.js";
 
 const containerStyle = {
   width: '100%',
@@ -17,6 +18,10 @@ function GoogleMaps() {
   const [autocomplete, setAutocomplete] = useState(null)
   const [lat , setLat] = useState(40.750183)
   const [lng , setLng]  = useState(-73.983759)
+  const [zone, SetZone] = useState([]);
+  const [Cityid , SetCityId] = useState(265);
+  const [colors, setColors] = useState();
+  // const [zoom, setZoom] = useState(8)
 
   const center = {
     lat,
@@ -39,7 +44,7 @@ console.log({position})
 
  
   const onPlaceChanged = ()=>{
-    console.log("ADDRESS ",autocomplete.getPlace())
+    // console.log("ADDRESS ",autocomplete.getPlace())
     var places = autocomplete.getPlace()
     setLat(places.geometry.location.lat())
     setLng(places.geometry.location.lng())
@@ -67,7 +72,6 @@ console.log({position})
   // }) 
   // },[])
 
-
   // console.log(layers , "tiles data")
   // const layers = new MVTLayer({
   //       data : 'https://testing-api.zoneomics.com/tiles/zones?x=2416&y=3081&z=13&city_id=265'
@@ -78,27 +82,123 @@ console.log({position})
 
   // console.log(layers , "from mvt layer")
 
+  // const overlay = useMemo(
+  //   () =>
+  //     new GoogleMapsOverlay({
+  //       layers: [
+  //         new MVTLayer({
+  //           data: `https://testing-api.zoneomics.com/tiles/zones?x={x}&y={y}&z={z}&city_id=265`,
+  //           minZoom: 0,
+  //           maxZoom: 23,
+  //           getLineColor: [19, 180, 192],
+  //           getFillColor: [14, 130, 180],
+  //         }),
+  //       ]
+  //     }),
+  //   []
+  // );
+   useEffect(()=>{
+         async function getData(){
+           try{
+            const res=await axios.get('https://testing-api.zoneomics.com/cities/findByLatLng?lat='+lat+'&lng='+lng, )
+          // if(res?.data?.data)
+                  console.log(res.data)
+                  SetZone(res.data.data.zoneCode);
+                  SetCityId(res.data.data.id);
+           } catch(error){
+                console.log("Not found any zone data",error);
+           }
+         }
+         getData()
+   },[lat,lng]);
+  
+
+  // const colors = 'red'
+  
+  // useEffect(() => {
+  //   async function getData() {
+  //     try {
+  //       // if (data[1] !== "") {
+  //         // const res = await axios.get(
+  //         //   "/cities/findByLatLng?lat=" + lat + "&lng=" + lng
+  //         // );
+  //         const res = axios.get('https://testing-api.zoneomics.com/cities/findByLatLng?lat=' + lat + '&lng=' + lng).then(
+  //           (res)=>console.log('api',{res})
+  //           )
+  //         // 
+  //         // console.log(res.data.data[0].zoneCode , "zone code")
+  //         // setShowResults(true);
+  //         // setIsOpen(true);
+  //       // }
+  //     } catch (e) {
+  //       console.log("NOT FOUND ANY RESULT", e);
+  //     }
+  //   }
+  //   getData();
+  // }, [lat, lng]);
+
+
+  // const generateColor = () => {
+  //   const randomColor = Math.floor(Math.random() * 16777215)
+  //     .toString(16)
+  //     .padStart(6, '0');
+  //   return `#${randomColor}`;
+  // };
+  useEffect(() => {
+    setColors(staticColor);
+  }, []);
+
+  // const matchExpression = ["match", ["get", "z"]];
+  // for (let row = 0; row < zone.length; row++) {
+  //   const color = colors[row];
+  //   matchExpression.push(zone[row], color);
+  // }
+  // matchExpression.push("white");
+
+    for ( let row = 0 ; row < zone.length ; row++){
+         const color = colors[row];
+        //  console.log(color)
+    }
+
+
   const deckOverlay = new GoogleMapsOverlay({
     layers: [
       new MVTLayer({
-        data: `https://testing-api.zoneomics.com/tiles/zones?x={x}&y={y}&z={z}&city_id=265`,
+        data: `https://testing-api.zoneomics.com/tiles/zones?x={x}&y={y}&z={z}&city_id=${Cityid}`,
         minZoom: 0,
         maxZoom: 23,
-        getLineColor: [192, 192, 192],
-        getFillColor: [140, 170, 180],
+        getLineColor: [19, 180 , 192],
+        getFillColor: [20, 200 , 168],
       }),
     ]
   });
-  
 
-  return isLoaded ? (
+
+  useEffect(()=>{
+    console.log({deckOverlay})
+    deckOverlay.setMap(map); 
+  },[Cityid])
+
+ 
+
+  //  const overlay = deckOverlay.setMap(map);
+  // useEffect(() => {
+  //   if (map) {
+  //     // map.setCenter(center);
+  //     // map.setZoom(14);
+  //     overlay.setMap(map);
+  //   }
+  // }, [map, center,zoom,  overlay]);
+
+  return (
       <GoogleMap
       mapContainerStyle={containerStyle}
       center={center}
-      zoom={3}
+      zoom={10}
       // onLoad={onLoad}
-      onUnmount={onUnmount}
+      // onUnmount={onUnmount}
       onLoad={map => {
+
         deckOverlay.setMap(map); 
       }}
     > 
@@ -106,15 +206,14 @@ console.log({position})
       onLoad={(e)=>setAutocomplete(e)}
        onPlaceChanged={onPlaceChanged}
        restrictions={{country: "us"}}>
-        <input id='place-id' type='text'  />
+        <input id='place-id' type='text'/>
       </Autocomplete>
       <Marker 
       onLoad={onRendered}
        position={position}
       ></Marker>
     </GoogleMap>
-  ) : <>
-  </>
+  );
 }
 
 export default React.memo(GoogleMaps)
